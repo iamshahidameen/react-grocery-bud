38d9e02f3f89cec6d85989b4cdf016735c22ee64
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-01 22:32:03 +0500

    Added items to the list dynamically

--- src/List.js
+++ src/List.js
@@ -1,7 +1,21 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
-}
+import React from 'react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+const List = ({ name }) => {
+  return (
+    <>
+      <article className="grocery-item">
+        <p className="title">{name}</p>
+        <div className="btn-container">
+          <button type="button" className="edit-btn">
+            <FaEdit />
+          </button>
+          <button type="button" className="delete-btn">
+            <FaTrash />
+          </button>
+        </div>
+      </article>
+    </>
+  );
+};
 
-export default List
+export default List;
